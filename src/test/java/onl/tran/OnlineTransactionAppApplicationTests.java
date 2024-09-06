c56c89db13cec17a4d3fdd36df9387dcdc52dd07
Ibrohimjon2151 <ibrohimjonyursunov50@gma.com>
2024-09-06 23:29:09 +0500

    Initial commit

---
+++ src/test/java/onl/tran/OnlineTransactionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package onl.tran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTransactionAppApplicationTests {
+
+ @Test
+ void contextLoads() {
+ }
+
+}
